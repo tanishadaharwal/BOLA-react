dc0f46d15643c33bf73ab5b248a63d1af4aa2d04
tanisha <tanisha22daharwal@gmail.com>
2023-06-30 15:56:46 +0530

    alfaaz

--- src/components/EventGallery.js
+++ src/components/EventGallery.js
@@ -7,7 +7,7 @@ export default function EventGallery() {
     <div className=' pt-36 pb-5  lg:px-36 px-5 '>
       <h1 className='text-4xl px-1 text-white  font-bold mb-6'>Event Gallery📸</h1>
       
-      <div className="pl-4 sm:pl-0 grid md:grid-cols-2 sm:grid-cols-1 items-center lg:grid-cols-4 gap-4">
+      <div className="pl-6 sm:pl-0 grid md:grid-cols-2 sm:grid-cols-1 items-center lg:grid-cols-4 gap-4">
         <img loading="lazy"  className='lg:h-60 lg:w-80 h-44 w-72 rounded-xl border-2 object-cover border-[#0cf790] hover:opacity-60 hover:scale-95 cursor-pointer duration-150' src="IMG_0422.jpg" alt="event-gallery" />
         <img loading="lazy"  className='lg:h-60 lg:w-80 h-44 w-72 rounded-xl border-2 object-cover border-[#0cf790] hover:opacity-60 hover:scale-95 cursor-pointer duration-150' src="IMG_6871.jpg" alt="event-gallery" />
         <img loading="lazy"  className='lg:h-60 lg:w-80 h-44 w-72 rounded-xl border-2 object-cover border-[#0cf790] hover:opacity-60 hover:scale-95 cursor-pointer duration-150' src="IMG_6896.jpg" alt="event-gallery" />
--- src/components/MessageGSBola.js
+++ src/components/MessageGSBola.js
@@ -11,12 +11,12 @@ export default function MessageGSBola() {
       
        
     
-        <div className='mt-8 h-1/2 lg:w-[70%] px-5 lg:text-xl text-sm text-[#ebecf3] font-semibold text-left leading-10'>
+        <div className='mt-8 h-1/2 lg:w-[70%]  px-5 lg:text-xl text-sm text-[#ebecf3] font-semibold text-left leading-10'>
         It is my utmost pleasure to extend a heartfelt welcome to all of you at our prestigious institution, which is enriched by a vibrant literary culture. We firmly believe that literature holds the power to transform the world. Here, at our college, we have a variety of clubs dedicated to fostering this spirit. I invite you to explore the diverse literary clubs and events offered by the Board of Literary Affairs. Embrace the opportunity to broaden your knowledge, nurture personal growth, and create cherished memories within our captivating environment. Once again, I warmly welcome you to IIT Ropar, where the journey of literary exploration begins.
 
       </div>
       <div className=''>
-      <img loading="lazy"  className="rounded-xl ml-10 lg:w-72 lg:h-80 w-40 h-72  hover:scale-110 duration-300 mt-8" alt="GS-BOLA"src="GS_BOLA.jpg"></img>
+      <img loading="lazy"  className="rounded-xl lg:ml-10 ml-14 lg:w-72 lg:h-80 w-40 h-60 object-cover hover:scale-110 duration-300 mt-8" alt="GS-BOLA"src="GS_BOLA.jpg"></img>
       <h2 className='text-lg mt-3 text-center lg:ml-0 -ml-10 text-[#ebecf3]'>Mr. Swaran Pratap Singh</h2>
       </div>
       

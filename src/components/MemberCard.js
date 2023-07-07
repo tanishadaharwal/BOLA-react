a7db2d95eb0a7d067001fdd8cc56cc07180c7d32
tanisha <tanisha22daharwal@gmail.com>
2023-07-08 00:00:02 +0530

    gallery

--- src/components/MemberCard.js
+++ src/components/MemberCard.js
@@ -20,7 +20,7 @@ export default function MemberCard({name, number, image, position}) {
                     <div className="mx-auto z-50  lg:h-60 lg:w-60 h-36 w-36 rounded-full">
                         <h1 className="text-xs lg:text-xl font-bold text-center mt-6 lg:mt-10">{name}</h1>
                         <h2 className="text-xs lg:text-xl font-bold text-center lg:mt-4">{position}</h2>
-                        <h2 className="text-xs text-sm font-bold text-center lg:mt-4">{number}</h2>
+                        <h2 className="text-xs lg:text-sm font-bold text-center lg:mt-4">{number}</h2>
                         
                         <div className="flex justify-evenly mt-4">
                             
